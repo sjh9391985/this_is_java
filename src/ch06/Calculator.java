4bcdd442ea8974d09bfc12353fa556b579048da5
JaeHyeon Son <sjh9391985@gmail.com>
2022-03-15 21:07:17 +0900

    ch06 static 예제

---
+++ src/ch06/Calculator.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class Calculator {
+
+    static double pi = 3.141592;
+
+    static int plus(int x,  int y){
+        return x+y;
+    }
+
+    static int minus(int x, int y){
+        return x-y;
+    }
+
+
+}
