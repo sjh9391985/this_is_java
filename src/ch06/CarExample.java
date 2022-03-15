570d9c0993db1c656941de8dd00bf4a95c03bc30
JaeHyeon Son <sjh9391985@gmail.com>
2022-03-15 20:37:49 +0900

    source code commit

---
+++ src/ch06/CarExample.java
@@ -0,0 +1,12 @@
+package ch06;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car car1 = new Car("포르쉐");
+        Car car2 = new Car("벤츠");
+
+        car1.run();
+        car2.run();
+    }
+
+}
